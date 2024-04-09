334d36065c21ce053ffdc71fa0e82b08cc188c7e
DenisNiki <denis.nikiforovl1995@gmail.com>
2024-04-09 18:51:42 +0300

    код вынес в функицю и потом вызвал не поломав код с 52 по 82. Вынес 66 строку в функицию которая в перспективе будет писать полученные данные в таблицу и вызвать , теперь на 49 строке данная функция. Типизировал все функий

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-function loadPage($url)
+function loadPage(string $url): DOMDocument
 {
     $html = file_get_contents($url);
     $dom = new DOMDocument();
@@ -8,15 +8,14 @@ function loadPage($url)
     return $dom;
 }
 
-function parseData($dom)
+function parseData(DOMDocument $dom): array
 {
     $xpath = new DOMXPath($dom);
     $divsWithDataObjectID = $xpath->query('//div[@class="col col-main-info"]');
     $phoneLinks = $xpath->query('//a[@class="phone__link"]');
     $adsLinks = $xpath->query('//div[@class="col col-main-info"]//a[@class="name__link"]');
     $agentInfos = $xpath->query('//div[@class="col-xl-9 col-lg-9 col-12"]');
-
-    $combinedData = array();
+    $combinedData = [];
 
     foreach ($divsWithDataObjectID as $index => $div) {
         $name = isset($agentInfos[$index]) ? trim($agentInfos[$index]->textContent) : '';
@@ -25,57 +24,65 @@ function parseData($dom)
         $adLink = isset($adsLinks[$index]) ? 'https://gohome.by' . $adsLinks[$index]->getAttribute('href') : '';
 
         $email = '';
+
         if (isset($phoneLinks[$index])) {
-            $phoneLink = $phoneLinks[$index]->getAttribute('href');
             $emailElement = $xpath->query(
                 './/div[@class="w-phone"]//a[@class="phone__link email"]',
                 $phoneLinks[$index]
             );
-            if ($emailElement->length > 0) {
-                $email = $emailElement[0]->textContent;
-            }
+
+            $email = $emailElement->length > 0 ? $emailElement[0]->textContent : '';
         }
 
-        $combinedData[] = array(
+        $combinedData[] = [
             'name' => $name,
             'address' => $address,
             'phone' => $phone,
             'adLink' => $adLink,
-            'email' => $email
-        );
+            'email' => $email,
+        ];
     }
 
     return $combinedData;
 }
 
-// Основной код
-$url = 'https://gohome.by/rent/flat/one-day'; // URL страницы с объявлениями на сутки
-$currentPage = 1; // текущая страница
-$totalPages = 1; // общее количество страниц (по умолчанию 1)
-$counter = 1; // счетчик для нумерации
+function writeDataToTable(int $counter, array $data): void
+{
+    echo $counter . '. Имя: ' . $data['name'] . ' | Адрес: ' . $data['address'] . ' | <br> Номер телефона: ' . $data['phone'] . ' | Ссылка на объявление: ' . $data['adLink'] . ' | e-mail: ' . $data['email'] . "<br><br>";
+}
 
-while ($currentPage <= $totalPages) {
-    $currentUrl = $url . '?page=' . $currentPage; // формирование URL с номером страницы
+function scrapeData(string $url): void
+{
+    $currentPage = 1;
+    $totalPages = 1;
+    $counter = 1;
 
-    $dom = loadPage($currentUrl);
-    $combinedData = parseData($dom);
+    while ($currentPage <= $totalPages) {
+        $currentUrl = $url . '?page=' . $currentPage;
 
-    foreach ($combinedData as $data) {
-        echo $counter . '. Имя: ' . $data['name'] . ' | Адрес: ' . $data['address'] . ' | <br> Номер телефона: ' . $data['phone'] . ' | Ссылка на объявление: ' . $data['adLink'] . ' | e-mail: ' . $data['email'] . "<br><br>";
-        $counter++;
-    }
+        $dom = loadPage($currentUrl);
+        $combinedData = parseData($dom);
 
-    // Получение общего количества страниц
-    if ($currentPage === 1) {
-        $paginationLinks = $dom->getElementsByTagName('a');
-        foreach ($paginationLinks as $link) {
-            if (is_numeric($link->textContent)) {
-                $pageNumber = intval($link->textContent);
-                $totalPages = max($totalPages, $pageNumber);
+        foreach ($combinedData as $index => $data) {
+            writeDataToTable($counter, $data);
+            $counter++;
+        }
+
+        // Получение общего количества страниц
+        if ($currentPage === 1) {
+            $paginationLinks = $dom->getElementsByTagName('a');
+            foreach ($paginationLinks as $link) {
+                if (is_numeric($link->textContent)) {
+                    $pageNumber = intval($link->textContent);
+                    $totalPages = max($totalPages, $pageNumber);
+                }
             }
         }
-    }
 
-    $currentPage++;
+        $currentPage++;
+        sleep(2);
+    }
 }
-?>
+
+$url = 'https://gohome.by/rent/flat/one-day';
+scrapeData($url);
